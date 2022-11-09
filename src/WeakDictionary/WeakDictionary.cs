3a9a021ac40b8f4194d73add9423b1ee5fbc9072
Bernhard Häussermann <bernhard.haeussermann@digiata.com>
2022-11-09 08:42:48 +0200

    Widen framework-support by targeting .NET Standard 2.0

--- src/WeakDictionary/WeakDictionary.cs
+++ src/WeakDictionary/WeakDictionary.cs
@@ -1,90 +1,93 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.CompilerServices;
 
-namespace BernhardHaus.Collections.WeakDictionary;
-
-public class WeakDictionary<TKey, TValue> : IDictionary<TKey, TValue>
-    where TKey : notnull
-    where TValue : class
+namespace BernhardHaus.Collections.WeakDictionary
 {
-    private readonly Dictionary<TKey, WeakReference> internalDictionary = new();
-    private readonly ConditionalWeakTable<TValue, Finalizer> conditionalWeakTable = new();
-
-    public TValue this[TKey key]
+    public class WeakDictionary<TKey, TValue> : IDictionary<TKey, TValue>
+        where TValue : class
     {
-        get => (TValue)this.internalDictionary[key].Target!;
-        set
+        private readonly Dictionary<TKey, WeakReference> internalDictionary = new Dictionary<TKey, WeakReference>();
+        private readonly ConditionalWeakTable<TValue, Finalizer> conditionalWeakTable = new ConditionalWeakTable<TValue, Finalizer>();
+
+        public TValue this[TKey key]
         {
-            Remove(key);
-            Add(key, value);
-        }
-    }
-
-    public ICollection<TKey> Keys => this.internalDictionary.Keys;
-
-    public ICollection<TValue> Values => this.internalDictionary.Values.Select(r => (TValue)r.Target!).ToArray();
-
-    public int Count => this.internalDictionary.Count;
-
-    public bool IsReadOnly => false;
-
-    public void Add(TKey key, TValue value)
-    {
-        this.internalDictionary.Add(key, new WeakReference(value));
-        var finalizer = new Finalizer(key);
-        finalizer.ValueFinalized += k => Remove(k);
-        this.conditionalWeakTable.Add(value, finalizer);
-    }
-
-    public void Add(KeyValuePair<TKey, TValue> item) => Add(item.Key, item.Value);
-
-    public void Clear() => this.internalDictionary.Clear();
-
-    public bool Contains(KeyValuePair<TKey, TValue> item) => ((IDictionary<TKey, TValue>)this.internalDictionary).Contains(item);
-
-    public bool ContainsKey(TKey key) => this.internalDictionary.ContainsKey(key);
-
-    public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex) => ((IDictionary<TKey, TValue>)this.internalDictionary).CopyTo(array, arrayIndex);
-
-    public bool Remove(TKey key) => this.internalDictionary.Remove(key);
-
-    public bool Remove(KeyValuePair<TKey, TValue> item) => ((IDictionary<TKey, TValue>)this.internalDictionary).Remove(item);
-
-    public bool TryGetValue(TKey key, out TValue value)
-    {
-        if (this.internalDictionary.TryGetValue(key, out var valueReference))
-        {
-            value = (TValue)valueReference.Target!;
-            return true;
+            get => (TValue)this.internalDictionary[key].Target;
+            set
+            {
+                Remove(key);
+                Add(key, value);
+            }
         }
 
-        value = default!;
-        return false;
-    }
+        public ICollection<TKey> Keys => this.internalDictionary.Keys;
 
-    public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
-    {
-        return this.internalDictionary.Select(kvp => new KeyValuePair<TKey, TValue>(kvp.Key, (TValue)kvp.Value.Target!)).GetEnumerator();
-    }
+        public ICollection<TValue> Values => this.internalDictionary.Values.Select(r => (TValue)r.Target).ToArray();
 
-    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        public int Count => this.internalDictionary.Count;
 
-    private sealed class Finalizer
-    {
-        private readonly TKey valueKey;
+        public bool IsReadOnly => false;
 
-        public Finalizer(TKey valueKey)
+        public void Add(TKey key, TValue value)
         {
-            this.valueKey = valueKey;
+            this.internalDictionary.Add(key, new WeakReference(value));
+            var finalizer = new Finalizer(key);
+            finalizer.ValueFinalized += k => Remove(k);
+            this.conditionalWeakTable.Add(value, finalizer);
         }
 
-        ~Finalizer()
+        public void Add(KeyValuePair<TKey, TValue> item) => Add(item.Key, item.Value);
+
+        public void Clear() => this.internalDictionary.Clear();
+
+        public bool Contains(KeyValuePair<TKey, TValue> item) => ((IDictionary<TKey, TValue>)this.internalDictionary).Contains(item);
+
+        public bool ContainsKey(TKey key) => this.internalDictionary.ContainsKey(key);
+
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex) => ((IDictionary<TKey, TValue>)this.internalDictionary).CopyTo(array, arrayIndex);
+
+        public bool Remove(TKey key) => this.internalDictionary.Remove(key);
+
+        public bool Remove(KeyValuePair<TKey, TValue> item) => ((IDictionary<TKey, TValue>)this.internalDictionary).Remove(item);
+
+        public bool TryGetValue(TKey key, out TValue value)
         {
-            ValueFinalized?.Invoke(this.valueKey);
+            if (this.internalDictionary.TryGetValue(key, out var valueReference))
+            {
+                value = (TValue)valueReference.Target;
+                return true;
+            }
+
+            value = default;
+            return false;
         }
 
-        public event ValueFinalizedDelegate? ValueFinalized;
-    }
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            return this.internalDictionary.Select(kvp => new KeyValuePair<TKey, TValue>(kvp.Key, (TValue)kvp.Value.Target)).GetEnumerator();
+        }
 
-    private delegate void ValueFinalizedDelegate(TKey valueKey);
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        private sealed class Finalizer
+        {
+            private readonly TKey valueKey;
+
+            public Finalizer(TKey valueKey)
+            {
+                this.valueKey = valueKey;
+            }
+
+            ~Finalizer()
+            {
+                ValueFinalized?.Invoke(this.valueKey);
+            }
+
+            public event ValueFinalizedDelegate ValueFinalized;
+        }
+
+        private delegate void ValueFinalizedDelegate(TKey valueKey);
+    }
 }
--- tests/WeakDictionary.Tests/TestWeakDictionary.cs
+++ tests/WeakDictionary.Tests/TestWeakDictionary.cs
@@ -1,38 +1,42 @@
-﻿namespace BernhardHaus.Collections.WeakDictionary.Tests;
+﻿using System;
+using NUnit.Framework;
 
-[TestFixture]
-internal class TestWeakDictionary
+namespace BernhardHaus.Collections.WeakDictionary.Tests
 {
-    [Test]
-    public void RemoveGarbageCollectedEntries()
+    [TestFixture]
+    internal class TestWeakDictionary
     {
-        var v1 = new ValueType();
-        var v2 = new ValueType();
-        var v3 = new ValueType();
+        [Test]
+        public void RemoveGarbageCollectedEntries()
+        {
+            var v1 = new ValueType();
+            var v2 = new ValueType();
+            var v3 = new ValueType();
 
-        var dictionary = new WeakDictionary<int, ValueType>
+            var dictionary = new WeakDictionary<int, ValueType>
             {
                 { 1, v1 },
                 { 2, v2 },
                 { 3, v3 }
             };
 
-        var weakReference = new WeakReference(v2);
-        v2 = null;
+            var weakReference = new WeakReference(v2);
+            v2 = null;
 
-        // Invoke garbage collection within a loop so as to avoid the call to be removed by compiler optimizations.
-        for (int i = 0; i < 1; i++)
-        {
-            GC.Collect();
+            // Invoke garbage collection within a loop so as to avoid the call to be removed by compiler optimizations.
+            for (int i = 0; i < 1; i++)
+            {
+                GC.Collect();
+            }
+
+            Assert.That(weakReference.IsAlive, Is.False);
+            CollectionAssert.AreEquivalent(new int[] { 1, 3 }, dictionary.Keys, "Unexpected keys after garbage collection.");
+
+            // Ensure these references stay alive and are not nulled by compiler optimizations.
+            v1.ToString();
+            v3.ToString();
         }
 
-        Assert.That(weakReference.IsAlive, Is.False);
-        CollectionAssert.AreEquivalent(new int[] { 1, 3 }, dictionary.Keys, "Unexpected keys after garbage collection.");
-
-        // Ensure these references stay alive and are not nulled by compiler optimizations.
-        v1.ToString();
-        v3.ToString();
+        private class ValueType { }
     }
-
-    private class ValueType { }
 }
